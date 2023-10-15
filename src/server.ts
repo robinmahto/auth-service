28d56a129ce79d438ec041ed5cd291532d2cfa42
Robin Mahto <58412832+robinmahto@users.noreply.github.com>
2023-10-15 13:57:02 +0000

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
-function login(username: string): boolean {
-    return true;
+function login(username: string): string {
+    const person = {
+        name:"robin"
+    }
+    const user = person.name
+    return username + user;
 }
 
 login('robin');
