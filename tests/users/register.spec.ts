01350a73dd3f2e95949429f8fd1b2bead5d99657
Robin Mahto <58412832+robinmahto@users.noreply.github.com>
2024-01-07 21:56:54 +0000

    testing user persistence

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { User } from '../../src/entity/User';
+import { DataSource } from 'typeorm';
+import { AppDataSource } from '../../src/config/data-source';
+import { truncateTables } from '../utils';
 
 describe('POST /auth/register', () => {
+    let connection: DataSource;
+    beforeAll(async () => {
+        connection = await AppDataSource.initialize();
+    });
+
+    beforeEach(async () => {
+        // Database truncate
+        await truncateTables(connection);
+    });
+
+    afterAll(async () => {
+        await connection.destroy();
+    });
+
     describe('Given all fields', () => {
         it('should return the 201 status code', async () => {
             // Arrange
@@ -35,7 +53,22 @@ describe('POST /auth/register', () => {
                 (response.headers as Record<string, string>)['content-type'],
             ).toEqual(expect.stringContaining('json'));
         });
-        it('should persist the user in the database', () => {});
+        it('should persist the user in the database', async () => {
+            // Arrange
+            const userData = {
+                firstName: 'Robin',
+                lastName: 'M',
+                email: 'robin@gmail.com',
+                password: 'secret',
+            };
+            // Act
+            await request(app).post('/auth/register').send(userData);
+
+            // assert
+            const userRepository = connection.getRepository(User);
+            const user = await userRepository.find();
+            expect(user).toHaveLength(1);
+        });
     });
     describe('Fields are missing', () => {});
 });
